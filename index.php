93eb6fbfeb6f6dbaa04e21bfdbfbf1b884e98c0e
vojislav <vojislavstevanovic171@gmail.com>
2022-11-17 12:53:28 +0100

    inicijalni komit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Životinje ZOO VRT</title>
+</head>
+
+<body>
+
+    <div class="index-div">
+
+        <div class="meni">
+            <a href="zivotinje.php"><button class="btn btn-light" id="dio_zivotinje">Dodaj/Izmeni/Obriši životinju</button></a>
+            <a href="pretrazisort.php"><button class="btn btn-light" id="dio_zivotinje">Pretraži/Sortiraj životinje</button></a>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
